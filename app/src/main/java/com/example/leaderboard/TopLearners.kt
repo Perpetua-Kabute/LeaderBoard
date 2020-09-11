960e07d99840da1261d259688f6298924abffa8c
Perpetua-Kabute <kabuteperpetua@gmail.com>
2020-09-11 21:31:13 +0300

    Feat:add data model classes

---
+++ app/src/main/java/com/example/leaderboard/TopLearners.kt
@@ -0,0 +1,8 @@
+package com.example.leaderboard
+
+data class TopLearners(
+    val name:String,
+    val hours:Int,
+    val country:String,
+    val badgeUrl:String
+)
---
+++ app/src/main/java/com/example/leaderboard/TopSkillIq.kt
@@ -0,0 +1,8 @@
+package com.example.leaderboard
+
+data class TopSkillIq(
+    val name:String,
+    val score:Int,
+    val country:String,
+    val badgeUrl:String
+)
