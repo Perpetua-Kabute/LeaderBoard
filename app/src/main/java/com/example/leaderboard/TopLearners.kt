3578e8bf40710bf61efd442a6a44f1d09e48e3c0
Perpetua-Kabute <kabuteperpetua@gmail.com>
2020-10-30 19:59:04 +0300

    fix: correct the data model

--- app/src/main/java/com/example/leaderboard/TopLearners.kt
+++ app/src/main/java/com/example/leaderboard/TopLearners.kt
@@ -1,6 +1,10 @@
 package com.example.leaderboard
 
+import com.google.gson.annotations.SerializedName
+
+
 data class TopLearners(
+
     val name:String,
     val hours:Int,
     val country:String,
--- app/src/main/java/com/example/leaderboard/TopskillLearnersService.kt
+++ app/src/main/java/com/example/leaderboard/TopskillLearnersService.kt
@@ -1,14 +1,15 @@
 package com.example.leaderboard
 
-import android.telecom.Call
+
 import kotlinx.coroutines.Deferred
+import retrofit2.Call
 import retrofit2.http.GET
 
 interface TopskillLearnersService {
 
     @GET("/api/hours")
-    fun getTopLearners():Deferred<List<TopLearners>>
+    fun getTopLearners(): Call<List<TopLearners>>
 
     @GET("/api/skilliq")
-    fun getTopSkillIq():Deferred<List<TopSkillIq>>
+    fun getTopSkillIq(): Call<List<TopSkillIq>>
 }
