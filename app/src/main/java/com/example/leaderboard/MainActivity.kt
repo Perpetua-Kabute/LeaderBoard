b158303b54e579649cebe8b0ec488f5b622c1836
Perpetua-Kabute <kabuteperpetua@gmail.com>
2020-10-30 20:06:02 +0300

    feat: Check lifecycle of activity

--- app/src/main/java/com/example/leaderboard/MainActivity.kt
+++ app/src/main/java/com/example/leaderboard/MainActivity.kt
@@ -5,13 +5,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.example.leaderboard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val SPLASH_TIME_OUT:Long = 9000
+        binding= DataBindingUtil.setContentView(this, R.layout.activity_main )
+        Log.i("Main Activity", "onCreate called")
+        val SPLASH_TIME_OUT:Long = 3000
 
        // window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
 
@@ -28,5 +33,30 @@ class MainActivity : AppCompatActivity() {
         }, SPLASH_TIME_OUT)
         }
 
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("Main Activity", "onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("Main Activity", "onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("Main Activity", "onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("Main Activity", "onStop called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("Main Activity", "onDestroy called")
     }
 }
