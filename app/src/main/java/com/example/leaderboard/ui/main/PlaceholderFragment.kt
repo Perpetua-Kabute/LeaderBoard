f7d684d6f33f4ee4fdb926bc61dee0dc92ebfb25
Perpetua-Kabute <kabuteperpetua@gmail.com>
2020-10-30 20:00:09 +0300

    fix: Get response as list

--- app/src/main/java/com/example/leaderboard/ui/main/PlaceholderFragment.kt
+++ app/src/main/java/com/example/leaderboard/ui/main/PlaceholderFragment.kt
@@ -1,18 +1,27 @@
 package com.example.leaderboard.ui.main
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.leaderboard.LearnerAdapter
-import com.example.leaderboard.R
+import com.example.leaderboard.*
+import com.example.leaderboard.ServiceBuilder.service
+import com.example.leaderboard.databinding.ActivityMainBinding
+import com.google.gson.Gson
 import kotlinx.android.synthetic.main.fragment_leader_board.*
+import kotlinx.coroutines.Deferred
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 /**
  * A placeholder fragment containing a simple view.
@@ -24,6 +33,8 @@ class PlaceholderFragment : Fragment() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
     private lateinit var viewManager: RecyclerView.LayoutManager
+    private lateinit var binding: ActivityMainBinding
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,20 +55,51 @@ class PlaceholderFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewManager = LinearLayoutManager(activity)
-        viewAdapter = LearnerAdapter()
-        recyclerView = recycler_view.apply {
-            setHasFixedSize(true)
-            layoutManager = viewManager
-            adapter = viewAdapter
 
 
-        }
+        val call:Call<List<TopLearners>> = service.getTopLearners()
+        call.enqueue(object: Callback<List<TopLearners>>{
+            override fun onResponse(call: Call<List<TopLearners>>, response: Response<List<TopLearners>>) {
+//                val gson: Gson = Gson()
+//                val learnerResultJson: String = gson.toJson(response.body())
+
+                if (response.isSuccessful) {
+                    val result = response.body()
+                    val gson: Gson = Gson()
+                    val learnerResultJson = gson.toJson(result)
+
+                    Log.d("Top Learners", learnerResultJson )
+                    Log.i("success", "Data fetch was successful")
+                    Log.i("First", "First fetch")
+                    viewManager = LinearLayoutManager(activity)
+                    viewAdapter = LearnerAdapter(result!!)
+                    recyclerView = recycler_view.apply {
+                        setHasFixedSize(true)
+                        layoutManager = viewManager
+                        adapter = viewAdapter
+                    }
+                }
+
+            }
+
+            override fun onFailure(call: Call<List<TopLearners>>, t: Throwable) {
+                Toast.makeText(activity, "Data fetch failed}", Toast.LENGTH_LONG).show()
+                Log.e("PlaceHolder", "${t.localizedMessage}");
+                Log.i("Failed", "did not fetch")
+            }
+            })
+
+
+
+
+
+
+
     }
 
     companion object {
         /**
-         * The fragment argument representing the section niumber for this
+         * The fragment argument representing the section number for this
          * fragment.
          */
         private const val ARG_SECTION_NUMBER = "section_number"
