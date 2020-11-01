068758d506178e150dfd50d39aa0f7b639f398d3
Perpetua-Kabute <kabuteperpetua@gmail.com>
2020-11-01 23:33:42 +0300

    feat: USe json convertor to create data model class

---
+++ app/src/main/java/com/example/leaderboard/Json/Response.kt
@@ -0,0 +1,3 @@
+package com.example.leaderboard.Json
+
+class Response : ArrayList<ResponseItem>()
---
+++ app/src/main/java/com/example/leaderboard/Json/ResponseItem.kt
@@ -0,0 +1,8 @@
+package com.example.leaderboard.Json
+
+data class ResponseItem(
+    val badgeUrl: String,
+    val country: String,
+    val hours: Int,
+    val name: String
+)
