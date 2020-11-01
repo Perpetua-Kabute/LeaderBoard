dcf52e372ac93c2b115e2c7b44065da12aa5c567
Perpetua-Kabute <kabuteperpetua@gmail.com>
2020-11-01 23:37:09 +0300

    feat: Add post request to interface

--- app/src/main/java/com/example/leaderboard/TopskillLearnersService.kt
+++ app/src/main/java/com/example/leaderboard/TopskillLearnersService.kt
@@ -1,9 +1,13 @@
 package com.example.leaderboard
 
 
+import com.example.leaderboard.ui.main.Posts
 import kotlinx.coroutines.Deferred
 import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.Field
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface TopskillLearnersService {
 
@@ -12,4 +16,14 @@ interface TopskillLearnersService {
 
     @GET("/api/skilliq")
     fun getTopSkillIq(): Call<List<TopSkillIq>>
+
+    @POST("")
+    fun postData(
+        @Field("name")name:String
+    )
+    @POST("")
+    fun postData2(
+        @Body data: Posts
+    ): Call<Posts>
+
 }
